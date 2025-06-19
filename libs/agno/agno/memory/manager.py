c251771f796ca9006b67de8e79bb8829636f1360
Willem Carel de Jongh <willemcarel@gmail.com>
2025-06-19 11:25:32 +0200

    Knowledge API, other memory refactoring

--- libs/agno/agno/memory/manager.py
+++ libs/agno/agno/memory/manager.py
@@ -3,9 +3,9 @@ from dataclasses import dataclass
 from textwrap import dedent
 from typing import Any, Callable, Dict, List, Optional
 
-from agno.memory.db.base import MemoryDb
 from agno.db.schemas.memory import MemoryRow
-from agno.memory.schema import UserMemory
+from agno.memory.db.base import MemoryDb
+from agno.memory import UserMemory
 from agno.models.base import Model
 from agno.models.message import Message
 from agno.tools.function import Function
